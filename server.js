9d24bc0aa4de9590f07717d001042e0a5f27e77e
Daksh-khetarpal <dakshisatbest@gmail.com>
2016-10-08 20:58:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {//if client makes request to url/ this functio
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {       //if this url is requested and this function is executed
+app.get('/ui/style.css', function (req, res) {       
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
